a70e39d80c419aaf993fdd34b6dcf7a8983dd7df
killyan35 <kilyan.gerard@3wa.io>
2023-01-04 14:26:52 +0000

    exo fonction en cours

---
+++ fonction/ex1/exercice1.js
@@ -0,0 +1,82 @@
+function add(nb1, nb2)
+{
+    
+    let sum = 0;
+    sum = nb1 + nb2;
+    
+    return sum;
+}
+
+function sub(nb1, nb2)
+{
+    let sum = 0;
+    sum = nb1 - nb2;
+    
+    return sum;
+}
+
+function mult(nb1, nb2)
+{
+    let sum = 0;
+    sum = nb1 * nb2;
+    
+    return sum;
+}
+
+function div(nb1, nb2)
+{
+    let sum = 0;
+    sum = nb1 / nb2;
+    
+    return sum;
+}
+
+function mod(nb1, nb2)
+{
+    let sum = 0;
+    sum = nb1 % nb2;
+    
+    return sum;
+}
+
+console.log(add(21,21));
+console.log(sub(21,21));
+console.log(mult(21,21));
+console.log(div(21,21));
+console.log(mod(21,21));
+
+
+function askUserOperation()
+{
+    let userOperation ;
+    userOperation = parseInt(window.prompt("Saisi une opération"));
+    if (userperation == ) 
+    {
+        
+    }
+    return userOperation ;
+}
+
+function askUserFirstNumber()
+{
+    let UserFirstNumber ;
+    UserFirstNumber = parseInt(window.prompt("Saisi un premier nombre"));
+    return UserFirstNumber ;
+}
+
+function askUserSecondNumber()
+{
+    let UserSecondNumber ;
+    UserSecondNumber = parseInt(window.prompt("Saisi un second nombre"));
+    return UserSecondNumber ;
+}
+
+console.log(askUserOperation());
+console.log(askUserFirstNumber());
+console.log(askUserSecondNumber());
+function calculator()
+{
+    
+}
+
+
